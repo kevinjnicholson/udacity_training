1fc4677827b065b73ffcc3b9c38336b0db8f90c7
Kev <106404588+kevinjnicholson@users.noreply.github.com>
2022-07-25 14:49:08 +0100

    Add functionality to enable all columns to be viewed

--- introduction_to_python/bikeshare.py
+++ introduction_to_python/bikeshare.py
@@ -9,6 +9,7 @@ from time import sleep
 import time
 import pandas as pd
 import numpy as np
+pd.set_option('display.expand_frame_repr', False)
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
